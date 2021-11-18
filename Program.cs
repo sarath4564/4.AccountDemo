46fba1c6cfec15f876cdd4ef926c1981949b4bf2
SARATHCHANDRA <sarathchandrak76@gmail.com>
2021-11-18 10:00:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace EventsEx01 
+{
+
+    public class TransactionEventArgs : EventArgs
+    {
+        public int TransactionAmount { get; set; }
+        public string TransactionType { get; set; }
+        public object amont { get; internal set; }
+
+        public TransactionEventArgs(int amount, string trans_type)
+        {
+            TransactionAmount = amount;
+            TransactionType = trans_type;
+        }
+    }
+    public delegate void TransactionHandler(object sender, TransactionEventArgs e); // delegate definition
+    public class Account
+    {
+        public event TransactionHandler TransactionMade; // Event Definition
+        private int m_balance_amount;
+        public int BalanceAmount
+        { get { return m_balance_amount; } }
+        public Account(int amount)
+        {
+            this.m_balance_amount = amount;
+        }
+
+        public void Debit(int amount)
+        {
+            if (amount < m_balance_amount)
+            {
+                m_balance_amount = m_balance_amount - amount;
+                TransactionEventArgs e = new TransactionEventArgs(amount, "Debited");
+                OnTransactionMade(e);
+            }
+        }
+        public void Credit(int amount)
+        {
+            m_balance_amount = m_balance_amount + amount;
+            TransactionEventArgs e = new TransactionEventArgs(amount, "Credited");
+            OnTransactionMade(e);
+        }
+        private void OnTransactionMade(TransactionEventArgs e)
+        {
+            if (TransactionMade != null)
+            {
+                TransactionMade(this, e); // Raise the event
+            }
+        }
+    }
+class Program
+    {
+        static void Main()
+        {
+            Account acnt1 = new Account(10000);
+            Account acnt2 = new Account(20000);
+            acnt1.TransactionMade += TransactionHolder;
+            acnt2.TransactionMade += TransactionHolder;
+            acnt1.Debit(2000);
+            acnt2.Credit(4000);
+          }
+        static void TransactionHolder(object sender,TransactionEventArgs e)
+        {
+            Console.WriteLine($"{e.amont} {e.TransactionType}");
+        }
+    }
+}
